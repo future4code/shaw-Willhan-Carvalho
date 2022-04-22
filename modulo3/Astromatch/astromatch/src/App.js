d2bf352afa9fc7008b6c212a02070fdb4d2914af
willmarques09 <willhanmarques09@gmail.com>
2022-04-22 12:14:16 -0300

    projeto Astromatch

--- modulo3/Astromatch/astromatch/src/App.js
+++ modulo3/Astromatch/astromatch/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Home from "./Components/Home"
+import MatchList from "./Components/MatchList"
 
 function App() {
+  const [tela, setTela] = useState ("Home")
+
+  const goToMatchList = () => {
+    setTela("MatchList")
+  }
+
+  const goToHome = () => {
+    setTela("Home")
+  }
+
+  const condicionalCabecalho = tela
+
+  switch (tela){
+    case "Home":
+      return <Home goToMatchList={goToMatchList} condicionalCabecalho={condicionalCabecalho}/>
+    case "MatchList":
+      return <MatchList goToHome={goToHome}/>
+    default:
+      return "ERRO"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+           
     </div>
   );
 }
